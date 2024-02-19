2f2425d7ae85240254e21fb7d0adab91ae0f0bfb
Subahan-1D <subahanislam523@gmail.com>
2024-02-19 11:49:14 +0600

    continue game

--- script.js
+++ script.js
@@ -9,23 +9,43 @@
 // // console.log(playGroundSection.classList)
 // }
 
-function continueGame(){
+
+// keyboard chapa chapi
+
+function keyBoardEnterButtonPress(event) {
+    const PlayerPressed = event.key;
+    console.log('Player Pressed',PlayerPressed);
+    // get the expected to press
+    const currentAlphabetGenerate = document.getElementById('current-alphabet')
+    const targetAlphabet = currentAlphabetGenerate.innerText;
+    const expected = targetAlphabet.toLowerCase()
+    console.log(PlayerPressed, expected);
+
+    if(PlayerPressed === expected){
+        console.log('Your Win Point')
+        console.log('You have press correctly ',expected)
+        removeBackgroundColorById(expected);
+        continueGame();
+    }else{
+        console.log('Your Lost Point , and Lost life')
+    }
+}
+// Capture keyboard key press
+document.addEventListener('keyup', keyBoardEnterButtonPress);
+// keyboard chapa chapi
+function continueGame() {
 
     const alphabet = getARandomAlphabets();
-    console.log('Your Random Alphabet : ',alphabet)
-
+    // console.log('Your Random Alphabet : ', alphabet)
     // Alphabet generate randomly show it
-
     const currentAlphabetGenerate = document.getElementById('current-alphabet')
-    currentAlphabetGenerate.innerText = alphabet ;
-
+    currentAlphabetGenerate.innerText = alphabet;
     // set background color 
-
     setBackgroundColorById(alphabet);
 
 }
 
-function play(){
+function play() {
     hideElementById('home-screen')
     showElementById('play-ground')
     continueGame()
